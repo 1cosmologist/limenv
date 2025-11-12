2ba7d20894dad0416528eeacca6c88d78114dd01
Shamik Ghosh <thequarkexpress@gmail.com>
2025-11-11 21:41:26 -0800

    Drop gpu support

--- conf/pip-pkgs.sh
+++ conf/pip-pkgs.sh
@@ -1,8 +1,8 @@
 # Install pip packages.
 echo Installing pip packages at $(date)
 
-# JAX install follows: https://docs.nersc.gov/development/languages/python/using-python-perlmutter/#jax
-pip install --no-cache-dir "jax[cuda12]==0.5.3" "jaxlib" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# # JAX install follows: https://docs.nersc.gov/development/languages/python/using-python-perlmutter/#jax
+pip install --no-cache-dir "jax[cpu]==0.5.3" "jaxlib" -f https://storage.googleapis.com/jax-releases/jax_releases.html
 
 # see https://docs.nersc.gov/development/languages/python/parallel-python/
 pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
